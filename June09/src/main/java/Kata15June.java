0fb8af0a10dc57d446769bd05630f590b4c1642e
AxelBankuna <axe.618@gmail.com>
2021-06-22 17:39:03 +0200

    completed getPeaks

--- June09/src/main/java/Kata15June.java
+++ June09/src/main/java/Kata15June.java
@@ -13,6 +13,7 @@ public class Kata15June {
         List<Integer> peakValues = new ArrayList<>();
         String peakPositions = "";
 
+        /*
         for (int i = 1; i < (arr.length-1); i++) {
             peak = arr[i];
             if (arr[i] > arr[i-1] && arr[i] > arr[i+1]) {
@@ -33,17 +34,33 @@ public class Kata15June {
                 }
             }
         }
+
+        */
+
+        for(int i = 1; i < arr.length - 1; i++){
+            if(isPeak(i, arr)){
+                peakValues.add(arr[i]);
+                if(peakPositions.isEmpty()){
+                    peakPositions += i +"";
+                }else{
+                    peakPositions += "," + i;
+                }
+            }
+
+        }
         peaks.put(peakPositions,peakValues);
         return peaks;
+
     }
 
     public static boolean isPeak(int pos, int[] arr) {
         if (arr[pos - 1] < arr[pos] && arr[pos + 1] < arr[pos])
             return true;
         if (arr[pos - 1] < arr[pos] && arr[pos + 1] == arr[pos]){
-            for (int i = pos + 1; i < arr.length - 2; i++) {
-                if (arr[i] > arr[pos])
+            for (int i = pos + 1; i < arr.length; i++) {
+                if (arr[i] > arr[pos]) {
                     return false;
+                }
                 if (arr[i] < arr[pos])
                     return true; // 111111123332
             }
@@ -53,9 +70,10 @@ public class Kata15June {
 
     public static void main(String[] args) {
 //        int[] arr = {0, 1, 2, 5, 1, 0};
-//        int[] arr = {3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3};
-        //[1, 2, 2, 2, 1]
-        int[] arr = {1, 2, 2, 2, 1, 2, 3, 3, 3, 3, 1};
+        int[] arr = {3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3};
+//        [1, 2, 2, 2, 1]
+//        int[] arr = {1, 2, 2, 2, 1, 2, 3, 3, 3, 3, 1};
+//        int[] arr = {1, 2, 2, 2, 2, 1};
         System.out.println(getPeaks(arr));
     }
 }
